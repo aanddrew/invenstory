d3b12720a91b8acfab551fa486c346307961bfd0
Andrew Weller <andrew@weller.fun>
2025-01-15 18:36:17 -0800

    Moving now or something LMAO I love git  xPPPP

--- characters/tori.gd
+++ characters/tori.gd
@@ -1,10 +1,14 @@
+class_name Tori
+
 extends CharacterBody2D
 
 var target_location: Vector2
-const SPEED = 200
+const SPEED := 100
 
-signal reached_target_location
-signal new_target_location(location)
+#signal reached_target_location
+#signal new_target_location(location)
+
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 const cardinal_directions = [
 	Vector2(0, 1),
@@ -24,28 +28,38 @@ func _ready() -> void:
 func _input(event):
 	pass
 
-
 func set_target_location(location):
-	target_location = location
-	new_target_location.emit(target_location)
+	#target_location = location
+	#new_target_location.emit(target_location)
 	
 	var diff = target_location - position
 	$Sprite2D.flip_h = diff.x < 10
+	
+	navigation_agent.target_position = location
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var diff = target_location - position
-	if diff.length() > 2:
-		var new_direction = cardinal_directions[0]
-		for direction in cardinal_directions:
-			if diff.dot(direction) > diff.dot(new_direction):
-				new_direction = direction
-				
-		velocity = new_direction * SPEED
-		move_and_slide()
+	pass
+	#var diff = target_location - position
+	#if diff.length() > 2:
+		#var new_direction = cardinal_directions[0]
+		#for direction in cardinal_directions:
+			#if diff.dot(direction) > diff.dot(new_direction):
+				#new_direction = direction
+				#
+		#velocity = new_direction * SPEED
+		#move_and_slide()
+#
+	#else:
+		#reached_target_location.emit()
+		#velocity = Vector2.ZERO
 
-	else:
-		reached_target_location.emit()
-		velocity = Vector2.ZERO
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
 
-		
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * SPEED
+	move_and_slide()
--- levels/level.gd
+++ levels/level.gd
@@ -1,13 +1,43 @@
 extends Node
 
-var tori
+@onready var tori: Tori = $Tori
+@onready var tile_map: TileMapLayer = $TileMapLayer
+#var navigable_tiles: Array[Vector2i]
+#var navigable_points: Array[Vector2]
+@onready var astar: AStar2D = AStar2D.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tori = get_node("Tori")
-	tori.connect("new_target_location", self.move_crosshair)
-	tori.connect("reached_target_location", self.hide_crosshair)
+	tori.navigation_agent.connect("navigation_finished", self.hide_crosshair)
 	get_node("Crosshair").visible = false
+	
+	var tiles : Array[Vector2i] = tile_map.get_used_cells()
+	var tile_id_lookup = {}
+	
+	print(tiles)
+	for i in range(0, tiles.size()):
+		var tile_coords : Vector2i = tiles[i]
+		var map_coords : Vector2 = tile_map.map_to_local(tile_coords)
+		#navigable_points.append(map_coords)
+		astar.add_point(i, map_coords)
+		tile_id_lookup[tile_coords] = i
+	
+	for i in range(0, tiles.size()):
+		var tile : Vector2i = tiles[i]
+		var directions := [
+			Vector2i.UP, Vector2i.DOWN, Vector2i.LEFT, Vector2i.RIGHT,
+			Vector2i.RIGHT + Vector2i.UP,
+			Vector2i.RIGHT + Vector2i.DOWN,
+			Vector2i.LEFT + Vector2i.UP,
+			Vector2i.LEFT + Vector2i.DOWN
+		]
+		
+		for dir : Vector2i in directions:
+			var check := tile + dir
+			var check_tile_id = tile_id_lookup.get(check)
+			
+			if check_tile_id != null:
+				astar.connect_points(i, check_tile_id)
 
 func move_crosshair(location):
 	get_node("Crosshair").visible = true
@@ -20,10 +50,19 @@ func hide_crosshair():
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton and event.is_pressed():
-		var target_location = get_viewport().get_camera_2d().get_global_mouse_position()
-		var tile_map : TileMapLayer = get_node("TileMapLayer")
-		#target_location.x = target_location.x % float(tile_map.tile_set.tile_size.x)
-		tori.set_target_location(target_location)
+		var try_location = get_viewport().get_camera_2d().get_global_mouse_position()
+		
+		var from := astar.get_closest_point(tori.position)
+		var to := astar.get_closest_point(try_location)
+		var path := astar.get_point_path(from, to)
+		
+		print(path)
+		
+		var to_position := astar.get_point_position(to)
+		tori.set_target_location(to_position)
+		#tori.navigation_agent.
+		move_crosshair(to_position)
+		print(to_position)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
